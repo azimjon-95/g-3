c96630c8a41dcdd75cbdd3713a9132d179da788e
IsmoilAbdulhayev07 <ismoilabdulhayev638@gmail.com>
2024-03-26 14:44:27 +0300

    Itis Supra

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.css'
 
+
 const Banner = () => {
     return (
         <div className='Banner'>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../banner/Banner'
+import Car from './components/car/Car'
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Car />
         </div>
     )
 }
