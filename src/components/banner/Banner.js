406de04db1184775513b8461194ad6b3b8b07972
azimjon-95 <>
2024-03-26 16:02:14 +0500

    Banner

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+const Banner = () => {
+    return (
+        <div className='Banner'>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+    )
+}
+
+export default Banner;
