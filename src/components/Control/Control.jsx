809736f40f1437e75f6868341af1945080615fd6
xurshidaoysha <xurshidarasulova123@gmail.com>
2024-03-28 15:53:05 +0500

    world

--- src/components/Control/Control.jsx
+++ src/components/Control/Control.jsx
@@ -4,12 +4,27 @@ import "./style.css"
 const Control = () => {
     return (
         <div>
-           
+
             <div className="container_control">
-            
-            <div className="box_control">
-            <h1>Lorem, ipsum.</h1>
-                </div>  
+                <div className="box_1">
+                <div className="box_control1"></div>
+                <div className="box_control2"></div>
+                <div className="box_control3"></div>
+                </div>
+                <div className="box_2">
+                <div className="box_control4"></div>
+                <div className="box_control5"></div>
+                <div className="box_control6"></div>
+                </div>
+                <div className="box_3">
+                <div className="box_control7"></div>
+                <div className="box_control8"></div>
+                <div className="box_control9"></div>
+                </div>
+
+
+
+
             </div>
         </div>
     )
